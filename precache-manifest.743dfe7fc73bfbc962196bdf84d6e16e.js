679bbb872560fe11ce77bf9bee28364b8d603b7a
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-10-05 01:31:27 +0300

    Updates

---
+++ precache-manifest.743dfe7fc73bfbc962196bdf84d6e16e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7558d1954695067ba9d952b7efff49b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "eab7890a7e406931a3f1",
+    "url": "/goit-react-hw-02-feedback/static/css/main.255228d3.chunk.css"
+  },
+  {
+    "revision": "0f86be08c50d1447e816",
+    "url": "/goit-react-hw-02-feedback/static/js/2.90b370f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.90b370f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eab7890a7e406931a3f1",
+    "url": "/goit-react-hw-02-feedback/static/js/main.0fbb1d07.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
